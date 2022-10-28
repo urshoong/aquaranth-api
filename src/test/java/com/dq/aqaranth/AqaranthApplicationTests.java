65566f6dc2aba2f16946a53f3422534539656006
ehek01 <whdgus0515@gmail.com>
2022-10-28 14:32:23 +0900

    fix projectname

---
+++ src/test/java/com/dq/aqaranth/AqaranthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dq.aqaranth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqaranthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
