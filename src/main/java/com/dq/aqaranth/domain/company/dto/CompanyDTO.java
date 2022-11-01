693c64ac8931b1ca91b3a7fc3166b744b5db6bfc
DoZerrro <ehdud1515@naver.com>
2022-11-01 22:18:15 +0900

    company 목록 구현 완료

---
+++ src/main/java/com/dq/aqaranth/domain/company/dto/CompanyDTO.java
@@ -0,0 +1,17 @@
+package com.dq.aqaranth.domain.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompanyDTO {
+    private Long companyNo;
+    private String companyName, companyAddress, companyTel, ownerName, businessNumber;
+    private LocalDate foundingDate;
+}
