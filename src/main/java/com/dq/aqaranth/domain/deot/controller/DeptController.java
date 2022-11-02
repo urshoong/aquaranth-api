62ae03bbdfa4c4ac6ec69f3e8106d2649be1f085
pgm1120 <106740326+pgm1120@users.noreply.github.com>
2022-11-02 15:17:48 +0900

    '첫번째커밋'

---
+++ src/main/java/com/dq/aqaranth/domain/deot/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.dq.aqaranth.domain.deot.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/api/dept")
+@RequiredArgsConstructor
+public class DeptController {
+
+}
