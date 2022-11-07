7076fad591bf1153853e3b83dc8c48d110f1ab12
Min Jun Kim <d0uwhs@gmail.com>
2022-11-07 10:19:54 +0900

    Feat: 메뉴 조회

--- src/main/java/com/dq/aquaranth/menu/mapper/MenuMapper.java
+++ src/main/java/com/dq/aquaranth/menu/mapper/MenuMapper.java
@@ -9,9 +9,6 @@ import java.util.List;
 public interface MenuMapper {
     List<MenuResponse> findAllMenus();
 
-//    Optional<List<MenuResponse>> findByEnableMenus();
-
     List<MenuResponse> findByGnBMenus();
-
 
 }
