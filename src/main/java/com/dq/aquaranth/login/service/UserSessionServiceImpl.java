551157226a4f8f8d453f0100968e957828a0d52b
ehek01 <whdgus0515@gmail.com>
2022-11-11 18:54:11 +0900

    aquaranth v1 db update

--- src/main/java/com/dq/aquaranth/login/service/UserSessionServiceImpl.java
+++ src/main/java/com/dq/aquaranth/login/service/UserSessionServiceImpl.java
@@ -1,33 +1,46 @@
 package com.dq.aquaranth.login.service;
 
 import com.dq.aquaranth.company.dto.CompanyDTO;
-import com.dq.aquaranth.login.repository.RedisRepository;
+import com.dq.aquaranth.company.mapper.CompanyMapper;
+import com.dq.aquaranth.dept.dto.DeptDTO2;
+import com.dq.aquaranth.dept.mapper.DeptMapper2;
+import com.dq.aquaranth.emp.dto.EmpDTO;
+import com.dq.aquaranth.login.domain.CustomUser;
+import com.dq.aquaranth.menu.mapper.MenuMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class UserSessionServiceImpl implements UserSessionService {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final CompanyMapper companyMapper;
+    private final DeptMapper2 deptMapper2;
+    private final MenuMapper menuMapper;
 
-
-    /**
-     * 로그인한 사용자의 회사정보
-     *
-     * @param username
-     * @return
-     */
     @Override
-    public CompanyDTO findCompanyByUsername(String username) {
-        ValueOperations<String, Object> test = redisTemplate.opsForValue();
-        Object obj = test.get(username);
-        log.info(obj);
+    public CustomUser findUserInfoInDatabase(EmpDTO empDTO) {
+        String username = empDTO.getUsername();
 
-        return null;
+        CompanyDTO companyDTO = companyMapper.findByUsername(username);
+        DeptDTO2 deptDTO2 = deptMapper2.findByUsername(username);
+        List<String> menuList = menuMapper.findMenusByLoginUsername(username);
+
+        log.error("@@@@@@@@@@@@@@@@@@@@@@@@2" + companyDTO);
+
+        return new CustomUser(companyDTO, deptDTO2, empDTO, menuList);
     }
 
+    @Override
+    public CustomUser findUserInfoInRedis(String username) {
+        ValueOperations<String, Object> test = redisTemplate.opsForValue();
+        log.info(test.get(username));
+        return (CustomUser) test.get(username);
+    }
 }
