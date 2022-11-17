2f82f27458e62c05f78d054efaa9b6f8b2accc6b
Min Jun Kim <d0uwhs@gmail.com>
2022-11-17 15:45:05 +0900

    Fix Conflict

--- src/main/java/com/dq/aquaranth/AquaranthApplication.java
+++ src/main/java/com/dq/aquaranth/AquaranthApplication.java
@@ -2,19 +2,18 @@ package com.dq.aquaranth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AquaranthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AquaranthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AquaranthApplication.class, args);
+    }
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
