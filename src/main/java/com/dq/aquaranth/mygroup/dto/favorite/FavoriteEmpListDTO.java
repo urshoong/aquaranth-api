c5aaeb92277c02e59a6c3a25be0b1cf6a829cb23
DoZerrro <ehdud1515@naver.com>
2022-11-29 21:58:11 +0900

    회사 기본정보 추가 시 로그인한 아이디 가져오기 수정완료 및 사원 정보 출력시 프로필 사진 가져오기 완료

--- src/main/java/com/dq/aquaranth/mygroup/dto/favorite/FavoriteEmpListDTO.java
+++ src/main/java/com/dq/aquaranth/mygroup/dto/favorite/FavoriteEmpListDTO.java
@@ -15,6 +15,6 @@ public class FavoriteEmpListDTO {
      * 해당 마이그룹에 즐겨찾기 된 모든 사원 정보를 담을 DTO
      * 마이그룹 번호, 조직 번호, 사원 이름, 직급, 아이디, 소속부서, 전화번호
      */
-    private Long orgaNo, mygroupNo;
+    private Long orgaNo, mygroupNo, empNo;
     private String empName, empRank, username, path, empPhone;
 }
