f7b0eb7870948339543ceca8965523600dee61c8
DoZerrro <ehdud1515@naver.com>
2022-11-29 23:11:23 +0900

    해당 사원이 이미 즐겨찾기 된 사원인지 확인하는 예외처리 구현 완료

--- src/main/java/com/dq/aquaranth/mygroup/service/favorite/FavoriteServiceImpl.java
+++ src/main/java/com/dq/aquaranth/mygroup/service/favorite/FavoriteServiceImpl.java
@@ -21,7 +21,27 @@ public class FavoriteServiceImpl implements FavoriteService {
     @Override
     public Long insert(FavoriteInsertDTO favoriteInsertDTO) {
         log.info("해당 마이그룹에 사원 즐겨찾기");
-        return favoriteeMapper.insert(favoriteInsertDTO);
+        Long insertResult = 0L; //즐겨찾기 성공 시 "1", 실패 시 "0"
+
+        //1. 해당 마이그룹 리스트 가져오기
+        List<FavoriteEmpListDTO> favoriteEmpListDTO
+                = favoriteeMapper.findAll(favoriteInsertDTO.getMygroupNo());
+
+        //2. 해당 마이그룹에 즐겨찾기 할 사원의 조직번호가 존재하는지 확인하기
+        Long favoriteEmpOrgaNo = favoriteInsertDTO.getOrgaNo();
+        for (FavoriteEmpListDTO empListDTO : favoriteEmpListDTO) {
+            if (empListDTO.getOrgaNo().equals(favoriteEmpOrgaNo)) {
+                //3-1. 해당 마이그룹에 즐겨찾기 실패
+                log.info("해당 사원은 이미 즐겨찾기 되어있습니다.");
+                log.info("즐겨찾기 실패결과: " + insertResult);
+                return insertResult;
+            }
+        }
+
+        //3. 해당 마이그룹에 즐겨찾기 성공
+        insertResult = favoriteeMapper.insert(favoriteInsertDTO);
+        log.info("즐겨찾기 성공결과: " + insertResult);
+        return insertResult;
     }
 
     @Override
