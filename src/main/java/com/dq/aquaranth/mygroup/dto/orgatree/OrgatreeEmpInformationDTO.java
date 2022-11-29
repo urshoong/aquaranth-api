5fd195bfd44e046fc9463a7353a820a18802485f
DoZerrro <ehdud1515@naver.com>
2022-11-29 18:08:08 +0900

    해당 부서에 속한 사원의 정보 출력 시 프로필 업로드 완료

--- src/main/java/com/dq/aquaranth/mygroup/dto/orgatree/OrgatreeEmpInformationDTO.java
+++ src/main/java/com/dq/aquaranth/mygroup/dto/orgatree/OrgatreeEmpInformationDTO.java
@@ -13,8 +13,9 @@ public class OrgatreeEmpInformationDTO {
 
     /**
      * 해당 부서에 소속된 사원 중 한 사원을 조회 후 정보를 담을 DTO
-     * 사원의 번호, 조직 번호, 이름, 직책, 아이디, 소속부서, 전화번호, 이메일
+     * 사원의 번호, 조직 번호, 이름, 직책, 아이디, 소속부서, 전화번호, 이메일, 프로필
      */
     private Long empNo, orgaNo;
     private String empName, empRank, username, path, empPhone, email;
+    private String filename, uuid, profileUrl;
 }
