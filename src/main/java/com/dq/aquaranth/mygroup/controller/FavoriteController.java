b0a786ebaa6e94df481d965afcda9089ac776faf
DoZerrro <ehdud1515@naver.com>
2022-12-01 20:52:36 +0900

    즐겨찾기 시 해당 아이디가 이미 존재하는지, 로그인한 사원을 즐겨찾기 하는지 체크 구현 완료

--- src/main/java/com/dq/aquaranth/mygroup/controller/FavoriteController.java
+++ src/main/java/com/dq/aquaranth/mygroup/controller/FavoriteController.java
@@ -30,7 +30,7 @@ public class FavoriteController {
         // 로그인한 사원의 아이디
         String username = authentication.getName();
         favoriteInsertDTO.setUsername(username);
-        return favoriteService.insert(favoriteInsertDTO);
+        return favoriteService.insert(favoriteInsertDTO, username);
     }
 
     @GetMapping("/list/{mygroupNo}")
