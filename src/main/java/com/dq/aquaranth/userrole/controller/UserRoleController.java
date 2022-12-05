f55757493bc6cf93e2e15eb1469a8e8cc1bf317a
urshoong <urshoong@gmail.com>
2022-12-05 16:51:39 +0900

    2022-12-05 UserRole Commit ## 작업내용 1. 조직도 부분 company_no 기준에서 orga_no 기준으로 변경 2. Controller 주석 추가 3. 권한그룹 등록 및 삭제 시 메시지 리턴 미처리 부분 완료

--- src/main/java/com/dq/aquaranth/userrole/controller/UserRoleController.java
+++ src/main/java/com/dq/aquaranth/userrole/controller/UserRoleController.java
@@ -26,60 +26,148 @@ public class UserRoleController {
 
     private final UserRoleService userRoleService;
 
-    /** GroupList **/
-
+    /**
+     * 모든 회사 정보를 조회합니다
+     *
+     * @return UserRoleCompanyDTO 의 List를 반환합니다
+     * @author 박준성
+     */
     @Operation(summary = "모든 회사 정보 조회", description = "사용 가능한 모든 회사 정보를 조회합니다.")
     @GetMapping("/companyListAll")
     public List<UserRoleCompanyDTO> findCompanyAll() {
         return userRoleService.findCompanyAll();
     }
 
+    /**
+     * 로그인한 사용자의 회사 정보를 조회합니다
+     * @param authentication - 로그인한 사용자의 정보를 담은 객체
+     * @return UserRoleCompanyDTO 의 List를 반환합니다
+     * @author 박준성
+     */
     @Operation(summary = "회사 정보 조회", description = "로그인한 사용자의 계정 정보를 사용하여 회사 정보를 조회합니다.")
     @GetMapping("/companyList")
     public List<UserRoleCompanyDTO> findCompany(Authentication authentication){
         return userRoleService.findCompany(authentication.getName());
     }
+
+    /**
+     * 선택된 회사의 권한그룹 목록을 조회합니다.
+     * @param pageRequestDTO - 페이지 정보 및 검색정보(page, size, keyword)를 담고 있는 객체
+     * @return UserRoleCompanyDTO 의 List를 반환합니다
+     * @author 박준성
+     */
     @Operation(summary = "권한 그룹 목록 조회", description = "선택한 회사에 부여 가능한 모든 권한그룹의 목록을 조회합니다.")
     @GetMapping("/roleGroupList")
     public PageResponseDTO<UserRoleRoleGroupBasedListDTO> findRoleGroupByCompanyNo(PageRequestDTO pageRequestDTO) {
         return userRoleService.findRoleGroupByOrgaNo(pageRequestDTO);
     }
 
+    /**
+     * 선택된 권한그룹이 부여된 조직(회사/부서/사원) 의 목록을 조회합니다.
+     * @param pageRequestDTO - 페이지 정보 및 검색정보(page, size, keyword)를 담고 있는 객체
+     * @return UserRoleCompanyDTO 의 List를 반환합니다
+     * @author 박준성
+     */
     @Operation(summary = "조직(회사/부서/사원) 목록 조회", description = "선택한 권한그룹이 부여된 조직(회사/부서/사원) 목록을 조회합니다.")
     @GetMapping("/roleGroupUserList")
     public PageResponseDTO<UserRoleGroupBasedUserListDTO> findOrgaByRoleGroupNo(PageRequestDTO pageRequestDTO) {
         return userRoleService.findOrgaByRoleGroupNo(pageRequestDTO);
     }
 
+    /**
+     * 권한그룹을 선택하고 조직(회사/부서/사원) 팝업에서 전달받은 정보를 통하여 권한그룹을 부여합니다.
+     * @param userRoleReqInsertOrgaRoleDTO - 권한그룹번호, 권한그룹을 부여할 조직(회사/부서/사원)의 orga_no 목록 전달
+     * @param authentication - 로그인한 사용자의 정보를 담은 객체
+     * @return UserRoleResponseDTO - 결과의 상태(state), 제목(title), 메시지(message)를 담은 객체 반환
+     * @author 박준성
+     */
     @Operation(summary = "권한그룹 부여", description = "공통 조직도에서 선택한 조직(회사/부서/사원)에 선택한 권한그룹을 부여합니다.")
     @PostMapping("/insertOrgaRole")
-    public Integer insertOrgaRole(@RequestBody UserRoleReqInsertOrgaRoleDTO userRoleReqInsertOrgaRoleDTO, Authentication authentication) {
+    public UserRoleResponseDTO insertOrgaRole(@RequestBody UserRoleReqInsertOrgaRoleDTO userRoleReqInsertOrgaRoleDTO, Authentication authentication) {
         userRoleReqInsertOrgaRoleDTO.setUsername(authentication.getName());
-        return userRoleService.insertUserRole(userRoleReqInsertOrgaRoleDTO);
+
+        UserRoleResponseDTO result;
+        try{
+            result = userRoleService.insertUserRole(userRoleReqInsertOrgaRoleDTO);
+        }catch(RuntimeException re){
+            result = UserRoleResponseDTO
+                    .builder()
+                    .state("error")
+                    .title("등록 실패")
+                    .message(re.getMessage())
+                    .build();
+        }catch(Exception e){
+            result = UserRoleResponseDTO
+                    .builder()
+                    .state("error")
+                    .title("등록 실패")
+                    .message("관리자에게 문의해주세요.")
+                    .build();
+        }
+        return result;
     }
 
+    /**
+     * 선택된 조직(회사/부서/사원)의 권한그룹을 해제합니다.
+     * @param userRoleReqRemoveOrgaRoleDTO - 권한그룹번호, 권한그룹을 해제할 조직(회사/부서/사원)의 orga_no 목록 전달
+     * @return UserRoleResponseDTO - 결과의 상태(state), 제목(title), 메시지(message)를 담은 객체 반환
+     * @author 박준성
+     */
     @Operation(summary = "부여된 권한그룹 제거", description = "선택한 조직(회사/부서/사원)에 부여된 권한그룹을 제거합니다.")
     @PostMapping("/removeOrgaRole")
-    public Integer removeOrgaRole(@RequestBody UserRoleReqRemoveOrgaRoleDTO userRoleReqRemoveOrgaRoleDTO) {
-        log.info("removeOrgaRole");
-        log.info(userRoleReqRemoveOrgaRoleDTO);
-        return userRoleService.removeOrgaRole(userRoleReqRemoveOrgaRoleDTO);
+    public UserRoleResponseDTO removeOrgaRole(@RequestBody UserRoleReqRemoveOrgaRoleDTO userRoleReqRemoveOrgaRoleDTO) {
+        UserRoleResponseDTO result;
+        try{
+            result = userRoleService.removeOrgaRole(userRoleReqRemoveOrgaRoleDTO);
+        }catch(RuntimeException re){
+            result = UserRoleResponseDTO
+                    .builder()
+                    .state("error")
+                    .title("삭제 실패")
+                    .message(re.getMessage())
+                    .build();
+        }catch(Exception e){
+            result = UserRoleResponseDTO
+                    .builder()
+                    .state("error")
+                    .title("삭제 실패")
+                    .message("관리자에게 문의해주세요.")
+                    .build();
+        }
+        return result;
     }
 
-    /** UserList **/
-
+    /**
+     * 선택한 회사의 사용자 목록을 조회합니다.
+     * @param pageRequestDTO - 페이지 정보 및 검색정보(page, size, keyword)를 담고 있는 객체
+     * @return UserRoleUserListBasedDTO 의 List를 반환합니다
+     * @author 박준성
+     */
     @Operation(summary = "사용자 목록 조회", description = "선택한 회사에 소속된 모든 사용자의 목록을 조회합니다.")
     @GetMapping("/findUserListByOrgaNo")
     public PageResponseDTO<UserRoleUserListBasedDTO> findUserListByOrgaNo(PageRequestDTO pageRequestDTO){
         return userRoleService.findUserListByOrgaNo(pageRequestDTO);
     }
 
+    /**
+     * 선택한 사용자에 부여된 모든 권한그룹을 조회합니다.
+     * @param pageRequestDTO - 페이지 정보 및 검색정보(page, size, keyword)를 담고 있는 객체
+     * @return UserRoleUserBasedRoleGroupDTO 의 List를 반환합니다
+     * @author 박준성
+     */
     @Operation(summary = "권한그룹 목록 조회", description = "선택한 사용자에 부여된 모든 권한그룹을 조회합니다.")
     @GetMapping("/findRoleGroupByUser")
     public PageResponseDTO<UserRoleUserBasedRoleGroupDTO> findRoleGroupByUser(PageRequestDTO pageRequestDTO) {
         return userRoleService.findRoleGroupByUser(pageRequestDTO);
     }
 
+    /**
+     * 사원의 권한그룹을 해제합니다.
+     * 회사 또는 부서등의 상위에 부여된 권한그룹은 삭제되지 않습니다.
+     * @param removeData - 권한그룹을 해제할 사원의 orga_no 목록 전달
+     * @return UserRoleResponseDTO - 결과의 상태(state), 제목(title), 메시지(message)를 담은 객체 반환
+     * @author 박준성
+     */
     @Operation(summary = "부여된 권한그룹 제거", description = "선택한 사용자에 부여된 권한그룹 중 본인에게 부여된 권한그룹만 제거 가능합니다.")
     @PostMapping("/removeOrgaRoleByAllRole")
     public UserRoleResponseDTO removeUserRole(@RequestBody List<UserRoleReqRemoveUserRoleDTO> removeData){
@@ -88,7 +176,15 @@ public class UserRoleController {
             result = userRoleService.removeOrgaRole(removeData);
         }catch (RuntimeException re){
             result.setState("error");
+            result.setTitle("삭제 실패");
             result.setMessage(re.getMessage());
+        }catch(Exception e){
+            result = UserRoleResponseDTO
+                    .builder()
+                    .state("error")
+                    .title("삭제 실패")
+                    .message("관리자에게 문의해주세요.")
+                    .build();
         }
         return result;
     }
