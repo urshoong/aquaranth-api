286d4a42d3803addc807818313748eb466e23f46
DoZerrro <ehdud1515@naver.com>
2022-12-05 10:40:02 +0900

    등록자를 사원이름에서 사원아이디로 수정

--- src/main/java/com/dq/aquaranth/company/dto/CompanyInformationDTO.java
+++ src/main/java/com/dq/aquaranth/company/dto/CompanyInformationDTO.java
@@ -13,8 +13,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CompanyInformationDTO {
+
+    /**
+     * 회사 등록 시 필요한 정보를 담을 DTO
+     * 회사번호(auto increment), 조직번호(last insert id)
+     * 회사이름, 회사주소, 회사전화번호, 대표자명, 사업자번호, 등록자
+     * 설립일, 사용여부
+     */
     private Long companyNo, orgaNo;
-    private String companyName, companyAddress, companyTel, ownerName, businessNumber, username;
+    private String companyName, companyAddress, companyTel, ownerName, businessNumber, regUser;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate foundingDate;
     private Boolean companyUse;
