998a39af364498f42f82a8f12e9214aea05c4322
ehek01 <whdgus0515@gmail.com>
2022-12-15 15:21:23 +0900

    redis init

--- src/main/java/com/dq/aquaranth/InitRunner.java
+++ src/main/java/com/dq/aquaranth/InitRunner.java
@@ -1,22 +1,11 @@
 package com.dq.aquaranth;
 
-import com.dq.aquaranth.login.domain.LoginUser;
 import com.dq.aquaranth.login.service.RedisService;
-import com.dq.aquaranth.login.service.UserSessionService;
-import com.dq.aquaranth.menu.dto.request.MenuQueryDTO;
-import com.dq.aquaranth.menu.dto.response.MenuResponseDTO;
-import com.dq.aquaranth.menu.service.MenuConfigurationService;
-import com.dq.aquaranth.rolegroup.domain.RoleGroup;
-import com.dq.aquaranth.rolegroup.service.RoleGroupService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * application 실행시 처리되는 로직입니다
@@ -34,8 +23,7 @@ public class InitRunner implements ApplicationRunner {
      */
     @Override
     public void run(ApplicationArguments args) {
-//        TODO: 주석제거
-//        initRedis();
+        initRedis();
     }
 
     /**
